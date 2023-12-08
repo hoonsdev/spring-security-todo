5f982e8513475ee5db8135e3aa5a57ef5d9187b4
Taehoon <mysoul98@gmail.com>
2023-12-08 15:35:49 +0900

    todo 관련 repository 추가

---
+++ src/main/java/sesac/springsecuritytodo/repository/TodoRepository.java
@@ -0,0 +1,15 @@
+package sesac.springsecuritytodo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sesac.springsecuritytodo.entity.TodoEntity;
+
+// TodoRepository 인터페이스
+// - JpaRepository 인터페이스를 확장
+// - JpaRepository<TodoEntity, Long>
+//    - TodoEntity: 테이블에 매핑될 엔티티 클래스
+//    - Long: 엔티티의 pk 타입
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+
+}
